ed3cb37ada62dff398740d330e49ed8d06698c31
sadhu <c.yao@aftership.com>
2022-04-10 14:35:01 +0800

    fix 🐛: 重新展示回收后的 MsgItem, 还会播放动画.

--- app/src/main/java/com/rengwuxian/wecompose/ui/ChatPage.kt
+++ app/src/main/java/com/rengwuxian/wecompose/ui/ChatPage.kt
@@ -39,244 +39,246 @@ import kotlin.math.roundToInt
 
 @Composable
 fun ChatPage(modifier: Modifier = Modifier) {
-  val viewModel: WeViewModel = viewModel()
-  val offsetPercentX by animateFloatAsState(if (viewModel.chatting) 0f else 1f)
-  val chat = viewModel.currentChat
-  if (chat != null) {
-    Column(
-      modifier
-        .offsetPercent(offsetPercentX)
-        .background(WeComposeTheme.colors.background)
-        .fillMaxSize()
-    ) {
-      WeTopBar(chat.friend.name) {
-        viewModel.endChat()
-      }
-      var shakingTime by remember {
-        mutableStateOf(0)
-      }
-      Box(
-        Modifier
-          .background(WeComposeTheme.colors.chatPage)
-          .weight(1f)
-      ) {
-        Box(
-          Modifier
-            .alpha(WeComposeTheme.colors.chatPageBgAlpha)
-            .fillMaxSize()
+    val viewModel: WeViewModel = viewModel()
+    val offsetPercentX by animateFloatAsState(if (viewModel.chatting) 0f else 1f)
+    val chat = viewModel.currentChat
+    if (chat != null) {
+        Column(
+            modifier
+                .offsetPercent(offsetPercentX)
+                .background(WeComposeTheme.colors.background)
+                .fillMaxSize()
         ) {
-          Image(
-            painterResource(R.drawable.ic_bg_newyear_left), null,
-            Modifier
-              .align(Alignment.CenterStart)
-              .padding(bottom = 100.dp)
-          )
-          Image(
-            painterResource(R.drawable.ic_bg_newyear_top), null,
-            Modifier
-              .align(Alignment.TopEnd)
-              .padding(horizontal = 24.dp)
-          )
-          Image(
-            painterResource(R.drawable.ic_bg_newyear_right), null,
-            Modifier
-              .align(Alignment.BottomEnd)
-              .padding(vertical = 200.dp)
-          )
+            WeTopBar(chat.friend.name) {
+                viewModel.endChat()
+            }
+            var shakingTime by remember {
+                mutableStateOf(0)
+            }
+            Box(
+                Modifier
+                    .background(WeComposeTheme.colors.chatPage)
+                    .weight(1f)
+            ) {
+                Box(
+                    Modifier
+                        .alpha(WeComposeTheme.colors.chatPageBgAlpha)
+                        .fillMaxSize()
+                ) {
+                    Image(
+                        painterResource(R.drawable.ic_bg_newyear_left), null,
+                        Modifier
+                            .align(Alignment.CenterStart)
+                            .padding(bottom = 100.dp)
+                    )
+                    Image(
+                        painterResource(R.drawable.ic_bg_newyear_top), null,
+                        Modifier
+                            .align(Alignment.TopEnd)
+                            .padding(horizontal = 24.dp)
+                    )
+                    Image(
+                        painterResource(R.drawable.ic_bg_newyear_right), null,
+                        Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(vertical = 200.dp)
+                    )
+                }
+                val shakingOffset = remember {
+                    Animatable(0f)
+                }
+                LaunchedEffect(key1 = shakingTime) {
+                    if (shakingTime != 0) {
+                        shakingOffset.animateTo(
+                            0f,
+                            animationSpec = spring(0.3f, 600f),
+                            initialVelocity = -2000f
+                        )
+                    }
+                }
+                LazyColumn(
+                    Modifier
+                        .fillMaxSize()
+                        .offset(shakingOffset.value.dp, shakingOffset.value.dp)
+                ) {
+                    items(chat.msgs.size) { index ->
+                        val msg = chat.msgs[index]
+                        MessageItem(msg, shakingTime, chat.msgs.size - index - 1)
+                    }
+                }
+            }
+            ChatBottomBar(onBombClicked = {
+                viewModel.boom(chat)
+                shakingTime++
+            })
         }
-        val shakingOffset = remember {
-          Animatable(0f)
-        }
-        LaunchedEffect(key1 = shakingTime) {
-          if (shakingTime != 0) {
-            shakingOffset.animateTo(
-              0f,
-              animationSpec = spring(0.3f, 600f),
-              initialVelocity = -2000f
-            )
-          }
-        }
-        LazyColumn(
-          Modifier
-            .fillMaxSize()
-            .offset(shakingOffset.value.dp, shakingOffset.value.dp)
-        ) {
-          items(chat.msgs.size) { index ->
-            val msg = chat.msgs[index]
-            MessageItem(msg, shakingTime, chat.msgs.size - index - 1)
-          }
-        }
-      }
-      ChatBottomBar(onBombClicked = {
-        viewModel.boom(chat)
-        shakingTime++
-      })
     }
-  }
 }
 
 @Composable
 fun MessageItem(msg: Msg, shakingTime: Int, shakingLevel: Int) {
-  val shakingAngleBubble = remember { Animatable(0f) }
-  LaunchedEffect(key1 = shakingTime, block = {
-    if (shakingTime != 0) {
-      delay(shakingLevel.toLong() * 30)
-      shakingAngleBubble.animateTo(
-        0f,
-        animationSpec = spring(0.4f, 500f),
-        initialVelocity = 1200f / (1 + shakingLevel * 0.4f)
-      )
-    }
-  })
-  if (msg.from == User.Me) {
-    Row(
-      Modifier
-        .fillMaxWidth()
-        .padding(8.dp),
-      horizontalArrangement = Arrangement.End
-    ) {
-      val bubbleColor = WeComposeTheme.colors.bubbleMe
-      Text("${msg.text}",
-        Modifier
-          .graphicsLayer(
-            rotationZ = shakingAngleBubble.value,
-            transformOrigin = TransformOrigin(1f, 0f)
-          )
-          .drawBehind {
-            val bubble = Path().apply {
-              val rect = RoundRect(
-                10.dp.toPx(),
+    val shakingAngleBubble = remember { Animatable(0f) }
+    var currentShakingTime by remember { mutableStateOf(shakingTime) }
+    LaunchedEffect(key1 = shakingTime, block = {
+        if (shakingTime != 0 && shakingTime != currentShakingTime) {
+            currentShakingTime = shakingTime
+            delay(shakingLevel.toLong() * 30)
+            shakingAngleBubble.animateTo(
                 0f,
-                size.width - 10.dp.toPx(),
-                size.height,
-                4.dp.toPx(),
-                4.dp.toPx()
-              )
-              addRoundRect(rect)
-              moveTo(size.width - 10.dp.toPx(), 15.dp.toPx())
-              lineTo(size.width - 5.dp.toPx(), 20.dp.toPx())
-              lineTo(size.width - 10.dp.toPx(), 25.dp.toPx())
-              close()
-            }
-            drawPath(bubble, bubbleColor)
-          }
-          .padding(20.dp, 10.dp),
-        color = WeComposeTheme.colors.textPrimaryMe)
-      Image(
-        painterResource(msg.from.avatar),
-        contentDescription = msg.from.name,
-        Modifier
-          .graphicsLayer(
-            rotationZ = shakingAngleBubble.value * 0.6f,
-            transformOrigin = TransformOrigin(1f, 0f)
-          )
-          .size(40.dp)
-          .clip(RoundedCornerShape(4.dp))
-      )
+                animationSpec = spring(0.4f, 500f),
+                initialVelocity = 1200f / (1 + shakingLevel * 0.4f)
+            )
+        }
+    })
+    if (msg.from == User.Me) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            val bubbleColor = WeComposeTheme.colors.bubbleMe
+            Text("${msg.text}",
+                Modifier
+                    .graphicsLayer(
+                        rotationZ = shakingAngleBubble.value,
+                        transformOrigin = TransformOrigin(1f, 0f)
+                    )
+                    .drawBehind {
+                        val bubble = Path().apply {
+                            val rect = RoundRect(
+                                10.dp.toPx(),
+                                0f,
+                                size.width - 10.dp.toPx(),
+                                size.height,
+                                4.dp.toPx(),
+                                4.dp.toPx()
+                            )
+                            addRoundRect(rect)
+                            moveTo(size.width - 10.dp.toPx(), 15.dp.toPx())
+                            lineTo(size.width - 5.dp.toPx(), 20.dp.toPx())
+                            lineTo(size.width - 10.dp.toPx(), 25.dp.toPx())
+                            close()
+                        }
+                        drawPath(bubble, bubbleColor)
+                    }
+                    .padding(20.dp, 10.dp),
+                color = WeComposeTheme.colors.textPrimaryMe)
+            Image(
+                painterResource(msg.from.avatar),
+                contentDescription = msg.from.name,
+                Modifier
+                    .graphicsLayer(
+                        rotationZ = shakingAngleBubble.value * 0.6f,
+                        transformOrigin = TransformOrigin(1f, 0f)
+                    )
+                    .size(40.dp)
+                    .clip(RoundedCornerShape(4.dp))
+            )
+        }
+    } else {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Image(
+                painterResource(msg.from.avatar),
+                contentDescription = msg.from.name,
+                Modifier
+                    .graphicsLayer(
+                        rotationZ = -shakingAngleBubble.value * 0.6f,
+                        transformOrigin = TransformOrigin(0f, 0f)
+                    )
+                    .size(40.dp)
+                    .clip(RoundedCornerShape(4.dp))
+            )
+            val bubbleColor = WeComposeTheme.colors.bubbleOthers
+            Text("${msg.text}",
+                Modifier
+                    .graphicsLayer(
+                        rotationZ = -shakingAngleBubble.value,
+                        transformOrigin = TransformOrigin(0f, 0f)
+                    )
+                    .drawBehind {
+                        val bubble = Path().apply {
+                            val rect = RoundRect(
+                                10.dp.toPx(),
+                                0f,
+                                size.width - 10.dp.toPx(),
+                                size.height,
+                                4.dp.toPx(),
+                                4.dp.toPx()
+                            )
+                            addRoundRect(rect)
+                            moveTo(10.dp.toPx(), 15.dp.toPx())
+                            lineTo(5.dp.toPx(), 20.dp.toPx())
+                            lineTo(10.dp.toPx(), 25.dp.toPx())
+                            close()
+                        }
+                        drawPath(bubble, bubbleColor)
+                    }
+                    .padding(20.dp, 10.dp),
+                color = WeComposeTheme.colors.textPrimary)
+        }
     }
-  } else {
-    Row(
-      Modifier
-        .fillMaxWidth()
-        .padding(8.dp)
-    ) {
-      Image(
-        painterResource(msg.from.avatar),
-        contentDescription = msg.from.name,
-        Modifier
-          .graphicsLayer(
-            rotationZ = -shakingAngleBubble.value * 0.6f,
-            transformOrigin = TransformOrigin(0f, 0f)
-          )
-          .size(40.dp)
-          .clip(RoundedCornerShape(4.dp))
-      )
-      val bubbleColor = WeComposeTheme.colors.bubbleOthers
-      Text("${msg.text}",
-        Modifier
-          .graphicsLayer(
-            rotationZ = -shakingAngleBubble.value,
-            transformOrigin = TransformOrigin(0f, 0f)
-          )
-          .drawBehind {
-            val bubble = Path().apply {
-              val rect = RoundRect(
-                10.dp.toPx(),
-                0f,
-                size.width - 10.dp.toPx(),
-                size.height,
-                4.dp.toPx(),
-                4.dp.toPx()
-              )
-              addRoundRect(rect)
-              moveTo(10.dp.toPx(), 15.dp.toPx())
-              lineTo(5.dp.toPx(), 20.dp.toPx())
-              lineTo(10.dp.toPx(), 25.dp.toPx())
-              close()
-            }
-            drawPath(bubble, bubbleColor)
-          }
-          .padding(20.dp, 10.dp),
-        color = WeComposeTheme.colors.textPrimary)
-    }
-  }
 }
 
 @Composable
 fun ChatBottomBar(onBombClicked: () -> Unit) {
-  var editingText by remember { mutableStateOf("") }
-  Row(
-    Modifier
-      .fillMaxWidth()
-      .background(WeComposeTheme.colors.bottomBar)
-      .padding(4.dp, 0.dp)
-  ) {
-    Icon(
-      painterResource(R.drawable.ic_voice),
-      contentDescription = null,
-      Modifier
-        .align(Alignment.CenterVertically)
-        .padding(4.dp)
-        .size(28.dp),
-      tint = WeComposeTheme.colors.icon
-    )
-    BasicTextField(
-      editingText, { editingText = it },
-      Modifier
-        .weight(1f)
-        .padding(4.dp, 8.dp)
-        .height(40.dp)
-        .clip(MaterialTheme.shapes.small)
-        .background(WeComposeTheme.colors.textFieldBackground)
-        .padding(start = 8.dp, top = 10.dp, end = 8.dp),
-      cursorBrush = SolidColor(WeComposeTheme.colors.textPrimary)
-    )
-    Text(
-      "\uD83D\uDCA3",
-      Modifier
-        .clickable(onClick = onBombClicked)
-        .padding(4.dp)
-        .align(Alignment.CenterVertically),
-      fontSize = 24.sp
-    )
-    Icon(
-      painterResource(R.drawable.ic_add),
-      contentDescription = null,
-      Modifier
-        .align(Alignment.CenterVertically)
-        .padding(4.dp)
-        .size(28.dp),
-      tint = WeComposeTheme.colors.icon
-    )
-  }
+    var editingText by remember { mutableStateOf("") }
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .background(WeComposeTheme.colors.bottomBar)
+            .padding(4.dp, 0.dp)
+    ) {
+        Icon(
+            painterResource(R.drawable.ic_voice),
+            contentDescription = null,
+            Modifier
+                .align(Alignment.CenterVertically)
+                .padding(4.dp)
+                .size(28.dp),
+            tint = WeComposeTheme.colors.icon
+        )
+        BasicTextField(
+            editingText, { editingText = it },
+            Modifier
+                .weight(1f)
+                .padding(4.dp, 8.dp)
+                .height(40.dp)
+                .clip(MaterialTheme.shapes.small)
+                .background(WeComposeTheme.colors.textFieldBackground)
+                .padding(start = 8.dp, top = 10.dp, end = 8.dp),
+            cursorBrush = SolidColor(WeComposeTheme.colors.textPrimary)
+        )
+        Text(
+            "\uD83D\uDCA3",
+            Modifier
+                .clickable(onClick = onBombClicked)
+                .padding(4.dp)
+                .align(Alignment.CenterVertically),
+            fontSize = 24.sp
+        )
+        Icon(
+            painterResource(R.drawable.ic_add),
+            contentDescription = null,
+            Modifier
+                .align(Alignment.CenterVertically)
+                .padding(4.dp)
+                .size(28.dp),
+            tint = WeComposeTheme.colors.icon
+        )
+    }
 }
 
 fun Modifier.offsetPercent(offsetPercentX: Float = 0f, offsetPercentY: Float = 0f) =
-  this.layout { measurable, constraints ->
-    val placeable = measurable.measure(constraints)
-    layout(placeable.width, placeable.height) {
-      val offsetX = (offsetPercentX * placeable.width).roundToInt()
-      val offsetY = (offsetPercentY * placeable.height).roundToInt()
-      placeable.placeRelative(offsetX, offsetY)
+    this.layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+        layout(placeable.width, placeable.height) {
+            val offsetX = (offsetPercentX * placeable.width).roundToInt()
+            val offsetY = (offsetPercentY * placeable.height).roundToInt()
+            placeable.placeRelative(offsetX, offsetY)
+        }
     }
-  }
